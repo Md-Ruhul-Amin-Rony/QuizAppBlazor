4a16af98f27a2d552d2c38646d34d3581278584d
Md-Ruhul-Amin-Rony <adiar1234@yahoo.com>
2023-11-26 21:12:41 +0100

    adding AnswerController

---
+++ Server/Controllers/AnswerController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuizAppBlazor.Server.Data;
+using QuizAppBlazor.Shared;
+using System.Security.Claims;
+
+namespace QuizApplication.Server.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnswerController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public AnswerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public void Post(Answer answer)
+        {
+
+            var question = _context.Questions.Where(q => q.Id == answer.QuestionId).FirstOrDefault();
+
+            answer.IsCorrectAnswer = String.Equals(question.CorrectAnswer, answer.Text, StringComparison.OrdinalIgnoreCase);
+
+
+            answer.UserId = GetUserId();
+
+            var savedAnswer = _context.Answers.Where(q => (q.QuestionId == answer.QuestionId && q.UserId == answer.UserId)).FirstOrDefault();
+
+            if (savedAnswer == null)
+            {
+                _context.Answers.Add(answer);
+            }
+            else
+            {
+                savedAnswer.Text = answer.Text;
+                savedAnswer.IsCorrectAnswer = answer.IsCorrectAnswer;
+                _context.Entry(savedAnswer).State = EntityState.Modified;
+            }
+
+            _context.SaveChanges();
+
+
+        }
+
+        [HttpPut]
+        public void Put(Answer answer)
+        {
+            var question = _context.Questions.Where(q => q.Id == answer.QuestionId).FirstOrDefault();
+
+            if (question.CorrectAnswer == answer.Text)
+            {
+                answer.IsCorrectAnswer = true;
+            }
+            _context.Entry(answer).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+        private string? GetUserId()
+        {
+            return User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+    }
+}
